74052b9f43e85069be508b64d87489fc6d0440a5
fazemanuel <139392471+fazemanuel@users.noreply.github.com>
2025-10-13 16:20:42 -0500

    creacion adaptador de IMouseEvent

--- src/main/java/com/example/sudokupoe/Models/MouseAdapter.java
+++ src/main/java/com/example/sudokupoe/Models/MouseAdapter.java
@@ -1,4 +1,21 @@
 package com.example.sudokupoe.Models;
 
-public class MouseAdapter {
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MouseAdapter implements IMouseEvent {
+    @Override
+    public void clickEnTextField(MouseEvent eventoMouse, TextField celda) {
+        // Implementación vacía - las subclases pueden sobrescribir
+    }
+
+    @Override
+    public void mouseEntraTextField(MouseEvent eventoMouse, TextField celda) {
+        // Implementación vacía - las subclases pueden sobrescribir
+    }
+
+    @Override
+    public void mouseSaleTextField(MouseEvent eventoMouse, TextField celda) {
+        // Implementación vacía - las subclases pueden sobrescribir
+    }
 }
