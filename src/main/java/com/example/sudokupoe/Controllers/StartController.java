95bb8571ec4d6ddb8091039872dad12739a25ed5
fazemanuel <139392471+fazemanuel@users.noreply.github.com>
2025-10-13 16:22:48 -0500

    implementacion metodo boton inicio, pasa de la pantalla inicial a l juego

--- src/main/java/com/example/sudokupoe/Controllers/StartController.java
+++ src/main/java/com/example/sudokupoe/Controllers/StartController.java
@@ -1,8 +1,13 @@
 package com.example.sudokupoe.Controllers;
 
+import com.example.sudokupoe.Views.SudokuView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartController {
 
@@ -10,7 +15,14 @@ public class StartController {
     private Button JugarBoton;
 
     @FXML
-    void OnActionBotonInicioJuego(ActionEvent event) {
+    void OnActionBotonInicioJuego(ActionEvent event) throws IOException {
+        SudokuView sudokuView = SudokuView.getInstance();
+        sudokuView.show();
+
+        // Close stage
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage)source.getScene().getWindow();
+        stage.close();
 
     }
 
