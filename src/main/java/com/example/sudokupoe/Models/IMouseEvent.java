dcbe3668f96ab46eeac2abc73b99ae4a4ebe4ddc
fazemanuel <139392471+fazemanuel@users.noreply.github.com>
2025-10-13 16:20:21 -0500

    creacion interfaz IMouseEvent

--- src/main/java/com/example/sudokupoe/Models/IMouseEvent.java
+++ src/main/java/com/example/sudokupoe/Models/IMouseEvent.java
@@ -1,4 +1,10 @@
 package com.example.sudokupoe.Models;
 
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public interface IMouseEvent {
+    void clickEnTextField(MouseEvent eventoMouse, TextField celda);
+    void mouseEntraTextField(MouseEvent eventoMouse, TextField celda);
+    void mouseSaleTextField(MouseEvent eventoMouse, TextField celda);
 }
