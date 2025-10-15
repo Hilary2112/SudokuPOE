43cd7059785d605da55e4611255c6cd4af8113ed
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-15 17:40:59 -0500

    Implementacion inicial del generador del sudoku, usando la clase validador

--- src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
@@ -1,4 +1,19 @@
 package com.example.sudokupoe.Models;
 
+import java.util.Random;
+
 public class GeneradorSudokuModelo {
+
+    private static final int matrizTamano= 6;
+    private static final int altoPorBloque = 2;
+    private static final int anchoPorBloque = 3;
+
+    private ValidadorSudokuModelo validadorDelSudoku;
+    private Random random;
+
+    public GeneradorSudokuModelo (){
+        validadorDelSudoku = new ValidadorSudokuModelo();
+        random = new Random();
+    }
+
 }
