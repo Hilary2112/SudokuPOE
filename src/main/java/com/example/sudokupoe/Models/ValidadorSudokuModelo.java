47c805a6291b94822ba8810948f871c080a18c02
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-15 16:26:20 -0500

    Definir variables estaticas para poder manipular el codigo y en caso de necesitar un cambio no tener que cambiar una por una las funcionalidades donde se emplean

--- src/main/java/com/example/sudokupoe/Models/ValidadorSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/ValidadorSudokuModelo.java
@@ -1,4 +1,8 @@
 package com.example.sudokupoe.Models;
 
 public class ValidadorSudokuModelo {
+
+    private static final int matrizTamano= 6;
+    private static final int altoPorBloque = 2;
+    private static final int anchoPorBloque = 3;
  }
