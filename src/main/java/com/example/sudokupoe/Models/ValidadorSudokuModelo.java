5e2defa6e46d5fab9f54a34fd6d55786b3502536
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-15 16:31:07 -0500

    validar que un numero cumpla con los requerimientos de no repetirse en filas columnas y bloques

--- src/main/java/com/example/sudokupoe/Models/ValidadorSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/ValidadorSudokuModelo.java
@@ -5,4 +5,17 @@ public class ValidadorSudokuModelo {
     private static final int matrizTamano= 6;
     private static final int altoPorBloque = 2;
     private static final int anchoPorBloque = 3;
+
+    public boolean verficarSiElMovimientoEsValido(int[][] tablero,int fila, int columna, int numero){
+        if (!verificarNumeroEnFila(tablero,fila,numero)) {
+            return false;
+        }
+        if (!verificarNumeroEnColumna(tablero, columna, numero)){
+            return false;
+        }
+        if (!VerificarNumeroEnBloque(tablero,fila,columna,numero)){
+            return false;
+        }
+        return true;
+    }
  }
