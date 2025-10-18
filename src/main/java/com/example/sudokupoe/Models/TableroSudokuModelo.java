ab0258f6b5259f0924f5f0da284628032772e853
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-17 21:22:14 -0500

    cambios en tablero sudoku modelo para que se ajuste al funcionamiento del programa

--- src/main/java/com/example/sudokupoe/Models/TableroSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/TableroSudokuModelo.java
@@ -3,29 +3,36 @@ package com.example.sudokupoe.Models;
 
 public class TableroSudokuModelo {
 
-
+    private static final int matrizTamano = 6;
     private int[][] tablero;
-
+    private int[][] tableroNoEditable;
 
     public TableroSudokuModelo() {
-        tablero = new int[6][6];
+        tablero = new int[matrizTamano][matrizTamano];
+        tableroNoEditable = new int[matrizTamano][matrizTamano];
     }
 
+    public void setTablero(int[][] nuevoTableroSudoku) {
+        for (int i = 0; i < matrizTamano; i++) {
+            for (int j = 0; j < matrizTamano; j++) {
+                tablero[i][j] = nuevoTableroSudoku[i][j];
+                tableroNoEditable[i][j] = nuevoTableroSudoku[i][j];
+            }
+        }
+    }
 
     public int[][] getTablero(){
         return tablero;
     }
 
     public void colocarNumeroEnLaCelda(int fila, int columna, int numero) {
-        if (fila >= 0 && fila < 6 && columna >= 0 && columna < 6){
+        if (tableroNoEditable[fila][columna] == 0) {
             tablero[fila][columna] = numero;
         }
     }
-
-
-
+    
     public void eliminarNumeroEnLaCelda(int fila, int columna) {
-        if (fila >= 0 && fila < 6 && columna >= 0 && columna < 6){
+        if (tableroNoEditable[fila][columna] == 0) {
             tablero[fila][columna] = 0;
         }
     }
@@ -34,11 +41,8 @@ public class TableroSudokuModelo {
         return tablero[fila][columna];
     }
 
-    public boolean verificarCeldaVacia(int fila, int columna){
-        return tablero[fila][columna] == 0;
+    public boolean celdaEditable(int fila, int columna) {
+        return tableroNoEditable[fila][columna] == 0;
     }
 
-    public void limpiarTablero(){
-        tablero = new int[6][6];
-    }
 }
