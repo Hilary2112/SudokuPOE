ac3d0c3c7e7fd18d607eaf936e6eb549b2740a77
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-17 22:28:57 -0500

    metodos iniciales para generar el tablero llenar el tablero con funcione recursiva y guardarlo para luego usarlo en metodos relacionados a lo que se le muestra al final al usuario

--- src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
@@ -1,4 +1,61 @@
 package com.example.sudokupoe.Models;
 
+import java.util.Random;
+
 public class GeneradorSudokuModelo {
+    private static final int matrizTamano = 6;
+    private static final int altoPorBloque = 2;
+    private static final int anchoPorBloque = 3;
+    private Random random;
+    private ValidadorSudokuModelo validadorSudokuModelo;
+    private boolean[][] celdasIniciales;
+    private int[][] solucionCompleta; // Guarda la solución completa
+
+    public GeneradorSudokuModelo(){
+        random = new Random();
+        validadorSudokuModelo = new ValidadorSudokuModelo();
+        celdasIniciales = new boolean[matrizTamano][matrizTamano];
+        solucionCompleta = new int[matrizTamano][matrizTamano];
+    }
+
+    public int[][] generarTablero(){
+        int[][] tableroCompleto = new int[matrizTamano][matrizTamano];
+        reiniciarCeldasIniciales();
+
+        if (!llenarTablero(tableroCompleto)) {
+            return generarTablero();
+        }
+
+        for (int i = 0; i < matrizTamano; i++) {
+            for (int j = 0; j < matrizTamano; j++) {
+                solucionCompleta[i][j] = tableroCompleto[i][j];
+            }
+        }
+        return ocultarCeldasDeTableroCompletado(tableroCompleto);
+    }
+
+    private boolean llenarTablero(int[][] tablero){
+        for(int fila = 0; fila < matrizTamano; fila++){
+            for(int columna = 0; columna < matrizTamano; columna++){
+                if(tablero[fila][columna] == 0){
+                    int[] numeros = generarNumerosAleatorios();
+
+                    for(int numero : numeros){
+                        if(validadorSudokuModelo.verficarSiElMovimientoEsValido(tablero, fila, columna, numero)){
+                            tablero[fila][columna] = numero;
+
+                            if(llenarTablero(tablero)){
+                                return true;
+                            }
+
+                            tablero[fila][columna] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
