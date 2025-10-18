6d56c4dd9cf80ac8b31f30565a79b3852c33236a
Hilary2112 <hilary.herrera@correounivalle.edu.co>
2025-10-17 20:24:28 -0500

    Metodos para generar el tablero, llenar el tablero y llenar el tablero con ayuda de recursiones verificando movimientos validos

--- src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
+++ src/main/java/com/example/sudokupoe/Models/GeneradorSudokuModelo.java
@@ -10,10 +10,54 @@ public class GeneradorSudokuModelo {
 
     private ValidadorSudokuModelo validadorDelSudoku;
     private Random random;
+    private boolean[][] celdasIniciales;
+    private int[][] solucionCompleta;
 
     public GeneradorSudokuModelo (){
         validadorDelSudoku = new ValidadorSudokuModelo();
         random = new Random();
+        celdasIniciales = new boolean[matrizTamano][matrizTamano];
+        solucionCompleta = new int[matrizTamano][matrizTamano];
+    }
+
+    public int[][] generarTablero(){
+        int[][] tableroCompleto = new int[matrizTamano][matrizTamano];
+        reiniciarCeldasIniciales();
+
+        if (!llenarTablero(tableroCompleto)) {
+            return generarTablero();
+        }
+
+        for (int i = 0; i < matrizTamano; i++) {
+            for (int j = 0; j < matrizTamano; j++) {
+                solucionCompleta[i][j] = tableroCompleto[i][j];
+            }
+        }
+        return ocultarCeldasDeTableroCompletado(tableroCompleto);
+    }
+
+    private boolean llenarTablero(int[][] tablero){
+        for(int fila = 0; fila < matrizTamano; fila++){
+            for(int columna = 0; columna < matrizTamano; columna++){
+                if(tablero[fila][columna] == 0){
+                    int[] numeros = generarNumerosAleatorios();
+
+                    for(int numero : numeros){
+                        if(validadorSudokuModelo.verficarSiElMovimientoEsValido(tablero, fila, columna, numero)){
+                            tablero[fila][columna] = numero;
+
+                            if(llenarTablero(tablero)){
+                                return true;
+                            }
+
+                            tablero[fila][columna] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
